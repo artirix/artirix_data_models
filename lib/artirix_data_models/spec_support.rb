ad3321344319d6f4ad6063c2ff286f6e14b88fc0
Eduardo Turiño <eturino@artirix.com>
2014-12-29 02:58:21 +0100

    fix badly extracted specs

--- lib/artirix_data_models/spec_support.rb
+++ lib/artirix_data_models/spec_support.rb
@@ -1,4 +1,3 @@
 require_relative './spec_support/shared_examples'
-require_relative './spec_support/model_mock'
 require_relative './spec_support/fake_mode'
 require_relative './spec_support/gateway_mock'
