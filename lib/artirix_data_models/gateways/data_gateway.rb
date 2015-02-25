fa0bd4bb3caa20044bbb4fa545d113cbedbc6968
Eduardo Turiño <eturino@artirix.com>
2015-02-25 16:31:04 +0000

    feat(Connection): add post requests

--- lib/artirix_data_models/gateways/data_gateway.rb
+++ lib/artirix_data_models/gateways/data_gateway.rb
@@ -1,8 +1,9 @@
 class ArtirixDataModels::DataGateway
-  attr_reader :connection
+  attr_reader :connection, :post_as_json
 
-  def initialize(connection: nil)
-    @connection = connection || DefaultConnectionLoader.default_connection
+  def initialize(connection: nil, post_as_json: true)
+    @connection   = connection || DefaultConnectionLoader.default_connection
+    @post_as_json = !!post_as_json
   end
 
   def get(path, response_adaptor: nil, body: nil, fake: false, fake_response: nil)
@@ -14,6 +15,15 @@ class ArtirixDataModels::DataGateway
     parse_get_response(response_body, response_adaptor)
   end
 
+  def post(path, response_adaptor: nil, body: nil, fake: false, fake_response: nil)
+    if fake
+      response_body = fake_response
+    else
+      response_body = perform_post(path, body)
+    end
+    parse_post_response(response_body, response_adaptor)
+  end
+
   private
 
   def perform_get(path, body = nil)
@@ -23,7 +33,18 @@ class ArtirixDataModels::DataGateway
     elsif response.status.to_i == 404
       raise NotFound, path
     else
-      raise GatewayError, "path: #{path}, status: #{response.status}, body: #{response.body}"
+      raise GatewayError, "method: get, path: #{path}, status: #{response.status}, body: #{response.body}"
+    end
+  end
+
+  def perform_post(path, body = nil)
+    response = connect_post(path, body)
+    if response.success?
+      response.body
+    elsif response.status.to_i == 404
+      raise NotFound, path
+    else
+      raise GatewayError, "method: post, path: #{path}, status: #{response.status}, body: #{response.body}"
     end
   end
 
@@ -32,7 +53,18 @@ class ArtirixDataModels::DataGateway
       req.body = body_to_json body unless body.nil?
     end
   rescue Faraday::ConnectionFailed => e
-    raise ConnectionError, "path: #{path}, error: #{e}"
+    raise ConnectionError, "method: get, path: #{path}, error: #{e}"
+  end
+
+  def connect_post(path, body)
+    connection.post path do |req|
+      unless body.nil?
+        req.body = body_to_json body
+        req.headers['Content-Type'] = 'application/json'
+      end
+    end
+  rescue Faraday::ConnectionFailed => e
+    raise ConnectionError, "method: post, path: #{path}, error: #{e}"
   end
 
   def body_to_json(body)
@@ -44,7 +76,15 @@ class ArtirixDataModels::DataGateway
     end
   end
 
+  def parse_post_response(result, response_adaptor)
+    parse_common_response(result, response_adaptor)
+  end
+
   def parse_get_response(result, response_adaptor)
+    parse_common_response(result, response_adaptor)
+  end
+
+  def parse_common_response(result, response_adaptor)
     if result.present?
       parsed_response = Oj.load result, symbol_keys: true
     else
