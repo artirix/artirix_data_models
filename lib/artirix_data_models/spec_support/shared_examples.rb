b7e01f3f2ad4139d8379de6b2a57f7650e341d97
Eduardo Turiño <eturino@artirix.com>
2015-08-12 19:47:50 +0100

    chore: nocov

--- lib/artirix_data_models/spec_support/shared_examples.rb
+++ lib/artirix_data_models/spec_support/shared_examples.rb
@@ -1,6 +1,4 @@
-# :nocov:
 require_relative './shared_examples/has_attributes'
 require_relative './shared_examples/a_readonly_active_model_like_artirix_data_models'
 require_relative './shared_examples/an_artirix_data_model_model'
 require_relative './shared_examples/an_artirix_data_model_dao'
-# :nocov:
