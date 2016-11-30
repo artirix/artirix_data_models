d5365cfa38f74042d67fcde9821a053ca155966d
Eduardo Turiño <eturino@artirix.com>
2016-11-30 20:31:51 +0000

    travis

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,8 +2,8 @@ $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'bundler/setup'
 Bundler.setup
 
-require 'codeclimate-test-reporter'
-CodeClimate::TestReporter.start
+require 'simplecov'
+SimpleCov.start
 
 require 'rspec/given'
 
